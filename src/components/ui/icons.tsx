dbd82a27046c9016af3e994211560e0b41403029
Juan Ignacio De los Santos <juanignaciodelossantos01@gmail.com>
2025-05-30 20:52:59 -0300

    feat: added linkedin, github and globe icons to Icons component

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -1,5 +1,10 @@
 import { ArrowRight, Sparkles } from "lucide-react";
+import { FaGithub, FaLinkedin, FaGlobe } from "react-icons/fa";
+
 export const Icons = {
   sparkles: Sparkles,
   arrowRight: ArrowRight,
+  github: FaGithub,
+  linkedin: FaLinkedin,
+  globe: FaGlobe,
 };
